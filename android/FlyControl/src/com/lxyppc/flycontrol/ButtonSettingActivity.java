63cd7368630b67794eb0fd8c27c230180cc920b8
lxyppc@gmail.com <lxyppc@gmail.com@c46beb6f-bab3-396c-c16b-06770e749159>
2013-05-13 06:14:14 +0000

    Change the class of LinearLayout.LayoutParam

--- android/FlyControl/src/com/lxyppc/flycontrol/ButtonSettingActivity.java
+++ android/FlyControl/src/com/lxyppc/flycontrol/ButtonSettingActivity.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager.LayoutParams;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class ButtonSettingActivity extends Activity {

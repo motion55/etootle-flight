78504f41a0cc6c4295d88b70695c3169c9329bad
lxyppc@gmail.com <lxyppc@gmail.com@c46beb6f-bab3-396c-c16b-06770e749159>
2013-05-15 09:47:56 +0000

    Add support for get vectors

--- android/FlyControl/src/com/lxyppc/flycontrol/HorizonSettingActivity.java
+++ android/FlyControl/src/com/lxyppc/flycontrol/HorizonSettingActivity.java
@@ -1,86 +1,28 @@
 package com.lxyppc.flycontrol;
 
-import com.lxyppc.flycontrol.PIDSettingActivity.PIDParam;
-
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.InputType;
-import android.widget.EditText;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
 public class HorizonSettingActivity extends Activity {
-	public class HorizonParam{
-		HorizonParam(Context ctx){
-			w = new EditText(ctx);
-			x = new EditText(ctx);
-			y = new EditText(ctx);
-			z = new EditText(ctx);
-			w.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
-			x.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
-			y.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
-			z.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
-		}
-		EditText w;
-		EditText x;
-		EditText y;
-		EditText z;
-	}
-	private HorizonParam[] params = new HorizonParam[3];
-	private EditText[] edits = new EditText[4];
+	
+	private QuadParamSettingActivity.ParamEdit params;
 	private QuadParamSettingActivity.ParamButton buttons;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_horizon_setting);
 		LinearLayout tl = (LinearLayout)this.findViewById(R.id.horizon_param_layout);
-		String[] name = new String[]{
-			getString(R.string.quad_horizon),
-		};
-		
-		{
-			LinearLayout ll = new LinearLayout(this);
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-			lp.weight = 2;
-			TextView v1 = new TextView(this);
-			TextView v2 = new TextView(this);
-			TextView v3 = new TextView(this);
-			TextView v4 = new TextView(this);
-			TextView v5 = new TextView(this);
-			v1.setText(R.string.quad_horizon);
-			v2.setText(R.string.quad_horizon_w);
-			v3.setText(R.string.quad_horizon_x);
-			v4.setText(R.string.quad_horizon_y);
-			v4.setText(R.string.quad_horizon_z);
-			ll.addView(v1,lp);
-			ll.addView(v2,lp);
-			ll.addView(v3,lp);
-			ll.addView(v4,lp);
-			ll.addView(v5,lp);
-			tl.addView(ll);
-		}
-		for(int i=0;i<name.length;i++){
-			LinearLayout ll = new LinearLayout(this);
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-			lp.weight = 2;
-			TextView v1 = new TextView(this);
-			v1.setText(name[i]);
-			params[i] = new HorizonParam(this);
-			ll.addView(v1,lp);
-			ll.addView(params[i].w,lp);
-			ll.addView(params[i].x,lp);
-			ll.addView(params[i].y,lp);
-			ll.addView(params[i].z,lp);
-			tl.addView(ll);
-			
-			edits[i*4 + 0] = params[i].w;
-			edits[i*4 + 1] = params[i].x;
-			edits[i*4 + 2] = params[i].y;
-			edits[i*4 + 3] = params[i].z;
-		}
-		buttons = new QuadParamSettingActivity.ParamButton(this, 19, 4, edits);
+
+		params = new QuadParamSettingActivity.ParamEdit(this, 1, 4);
+		params.append2layout(tl, R.array.quad_horizon_param_head, R.array.quad_horizon_param_names);
+		buttons = new QuadParamSettingActivity.ParamButton(this, 19, 4, params);
 		buttons.append2layout(tl);
+	}
+	
+	public boolean onContextItemSelected(MenuItem item) {
+		params.onContextItemSelected(item);
+		return super.onContextItemSelected(item);
 	}
 	
 	protected void onResume(){
--- android/FlyControl/src/com/lxyppc/flycontrol/SensorSettingActivity.java
+++ android/FlyControl/src/com/lxyppc/flycontrol/SensorSettingActivity.java
@@ -1,128 +1,29 @@
 package com.lxyppc.flycontrol;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.InputType;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
 public class SensorSettingActivity extends Activity {
-	public class SensorParam{
-		public static final int COUNT = 3;
-		SensorParam(Context ctx, View.OnCreateContextMenuListener l){
-			for(int i=0;i<COUNT;i++){
-			edit[i] = new EditText(ctx);
-			edit[i].setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
-			edit[i].setOnCreateContextMenuListener(l);
-			}
-		}
-		EditText[] edit = new EditText[COUNT];
-	}
 
-	private static final int IDX_BASE = 10000;
-	private View.OnCreateContextMenuListener mCreateContextMenuListener= new View.OnCreateContextMenuListener(){
-		@Override
-		public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-			int index = -1;
-			for(int i=0;i<params.length;i++){
-				for(int j=0;j<params[0].edit.length;j++){
-					if(v == params[i].edit[j]){
-						index = i*3+j;
-						break;
-					}
-				}
-				if(index>0) break;
-			}
-			if(index > 0){
-				index += IDX_BASE;
-				menu.add(0, index, Menu.NONE, "X Fill");
-				menu.add(1, index, Menu.NONE, "Y Fill");
-			}
-		}
-	};
-	private SensorParam[] params = new SensorParam[6];
-	private EditText[] edits = new EditText[18];
+	private QuadParamSettingActivity.ParamEdit params;
 	private QuadParamSettingActivity.ParamButton buttons;
-	
-	public boolean onContextItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if(id>=IDX_BASE && id<IDX_BASE+18){
-			id -= IDX_BASE;
-			int row = id/SensorParam.COUNT;
-			int col = id%SensorParam.COUNT;
-			String v = params[row].edit[col].getText().toString();
-			if(item.getGroupId() == 0){
-				// X fill
-				for(int i=0;i<SensorParam.COUNT;i++){
-					params[row].edit[i].setText(v);
-				}
-			}else{
-				// Y fill
-				for(int i=0;i<params.length;i++){
-					params[i].edit[col].setText(v);
-				}
-			}
-		}
-		return super.onContextItemSelected(item);
-	}
-
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sensor_setting);
 		LinearLayout tl = (LinearLayout)this.findViewById(R.id.sensor_param_layout);
-		String[] name = new String[]{
-			getString(R.string.quad_acc_offset),
-			getString(R.string.quad_acc_gain),
-			getString(R.string.quad_gyro_offset),
-			getString(R.string.quad_gyro_gain),
-			getString(R.string.quad_mag_offset),
-			getString(R.string.quad_mag_gain),
-		};
-		{
-			LinearLayout ll = new LinearLayout(this);
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-			lp.weight = 2;
-			TextView v1 = new TextView(this);
-			TextView v2 = new TextView(this);
-			TextView v3 = new TextView(this);
-			TextView v4 = new TextView(this);
-			v1.setText(R.string.quad_sensor_cal);
-			v2.setText(R.string.quad_sensor_x);
-			v3.setText(R.string.quad_sensor_y);
-			v4.setText(R.string.quad_sensor_z);
-			ll.addView(v1,lp);
-			ll.addView(v2,lp);
-			ll.addView(v3,lp);
-			ll.addView(v4,lp);
-			tl.addView(ll);
-		}
-		for(int i=0;i<name.length;i++){
-			LinearLayout ll = new LinearLayout(this);
-			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-			lp.weight = 2;
-			TextView v1 = new TextView(this);
-			v1.setText(name[i]);
-			params[i] = new SensorParam(this, mCreateContextMenuListener);
-			ll.addView(v1,lp);
-			for(int j=0;j<SensorParam.COUNT;j++){
-				ll.addView(params[i].edit[j],lp);
-				edits[i*SensorParam.COUNT + j] = params[i].edit[j];
-			}
-			tl.addView(ll);
-		}
-		buttons = new QuadParamSettingActivity.ParamButton(this, 1, 18, edits);
+		
+		params = new QuadParamSettingActivity.ParamEdit(this, 6, 3);
+		params.append2layout(tl, R.array.quad_sensor_param_head, R.array.quad_sensor_param_names);
+		buttons = new QuadParamSettingActivity.ParamButton(this, 1, 18, params);
 		buttons.append2layout(tl);
+	}
+	
+	public boolean onContextItemSelected(MenuItem item) {
+		params.onContextItemSelected(item);
+		return super.onContextItemSelected(item);
 	}
 	
 	protected void onResume(){
